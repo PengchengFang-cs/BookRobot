b1da0ae85d97414750cee643d10c095fbd801b70
Fang Pengcheng <fangpengcheng0823@gmail.com>
2026-08-21 04:54:48 +0000

    fix: restore stable cart marker threshold

--- cart_geometry.py
+++ cart_geometry.py
@@ -179,7 +179,7 @@ def detect_cart_black_marker_pixels(
     *,
     color_bgr,
     observation,
-    gray_threshold=100,
+    gray_threshold=160,
 ):
     """Return back-left/back-right/front-left/front-right marker centers."""
 
@@ -268,7 +268,7 @@ def reconstruct_cart_marker_platform(
     slot_offsets_from_left_m=(0.17, 0.24, 0.31, 0.38, 0.45),
     platform_width_m=0.75,
     platform_depth_m=0.29,
-    gray_threshold=100,
+    gray_threshold=160,
     minimum_depth_m=0.20,
     maximum_depth_m=3.0,
 ):
